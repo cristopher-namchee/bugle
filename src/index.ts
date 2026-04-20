25bee829ef56b242cf6dade6d8523c4487d0bcf9
Cristopher <cristophernamchee12@gmail.com>
2026-04-20 20:21:44 +0700

    feat: re-integrate cloudflare workers as CRON scheduler

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import type { Env } from './types';
+
+const schedules: Record<string, (env: Env) => Promise<void>> = {
+  '0 3 * * *': async (env: Env) => {
+    await fetch(
+      'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/daily/dispatches',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+          'X-GitHub-Api-Version': '2026-03-10',
+        },
+        body: JSON.stringify({ ref: 'main' }),
+      },
+    );
+  },
+  '0 8 * * 6': async (env: Env) => {
+    await fetch(
+      'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/weekly/dispatches',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+          'X-GitHub-Api-Version': '2026-03-10',
+        },
+        body: JSON.stringify({ ref: 'main' }),
+      },
+    );
+  },
+};
+
+export default {
+  scheduled: async (
+    ctrl: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext,
+  ) => {
+    const task = schedules[ctrl.cron];
+
+    if (task) {
+      ctx.waitUntil(task(env));
+    }
+  },
+};
