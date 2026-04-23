be21ba57d7c5d83976f1a3760396858bc84d156c
Cristopher <cristopher@gdplabs.id>
2026-04-23 13:48:51 +0700

    refactor: better logging system

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,61 @@
-import { Hono } from 'hono';
 import type { Env } from './types';
 
 const schedules: Record<string, (env: Env) => Promise<void>> = {
   '0 3 * * *': async (env: Env) => {
-    const response = await fetch(
-      'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/daily.yml/dispatches',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${env.GITHUB_TOKEN}`,
-          'X-GitHub-Api-Version': '2026-03-10',
+    try {
+      const response = await fetch(
+        'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/daily.yml/dispatches',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+            'X-GitHub-Api-Version': '2026-03-10',
+          },
+          body: JSON.stringify({ ref: 'main' }),
         },
-        body: JSON.stringify({ ref: 'main' }),
-      },
-    );
+      );
 
-    const body = await response.json();
+      if (response.status !== 200) {
+        console.error(`Got ${response.status} from API.`);
+      }
 
-    console.log(body);
+      console.log('Successfully dispatched daily report flow.');
+    } catch (err) {
+      console.error(
+        `Error when dispatching actions: ${(err as Error).message}`,
+      );
+    }
   },
   '0 8 * * 6': async (env: Env) => {
-    const response = await fetch(
-      'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/weekly.yml/dispatches',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${env.GITHUB_TOKEN}`,
-          'X-GitHub-Api-Version': '2026-03-10',
+    try {
+      const response = await fetch(
+        'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/weekly.yml/dispatches',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+            'X-GitHub-Api-Version': '2026-03-10',
+          },
+          body: JSON.stringify({ ref: 'main' }),
         },
-        body: JSON.stringify({ ref: 'main' }),
-      },
-    );
+      );
 
-    const body = await response.json();
+      if (response.status !== 200) {
+        console.error(`Got ${response.status} from API.`);
+      }
 
-    console.log(body);
+      console.log('Successfully dispatched weekly report flow.');
+    } catch (err) {
+      console.error(
+        `Error when dispatching actions: ${(err as Error).message}`,
+      );
+    }
   },
 };
 
-const app = new Hono<{ Bindings: Env }>();
-
-app.get('/', async (c) => {
-  try {
-    const response = await fetch(
-      'https://api.github.com/repos/cristopher-namchee/bugle/actions/workflows/daily.yml/dispatches',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Bearer ${c.env.GITHUB_TOKEN}`,
-          'X-GitHub-Api-Version': '2026-03-10',
-        },
-        body: JSON.stringify({ ref: 'main' }),
-      },
-    );
-
-    const body = await response.json();
-
-    return c.json(body);
-  } catch (err) {
-    return c.json(
-      {
-        err,
-      },
-      { status: 400 },
-    );
-  }
-});
-
 export default {
-  fetch: app.fetch,
   scheduled: async (
     ctrl: ScheduledController,
     env: Env,
